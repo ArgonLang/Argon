5d9d2a73ea215e7206ba77a2d667eef3a9aa79a0
jacopodl <jacopo.delu@gmail.com>
2019-11-10 12:41:12 +0100

    Beginning Parser implementation

---
+++ src/lang/parser.h
@@ -0,0 +1,14 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#ifndef ARGON_LANG_PARSER_H_
+#define ARGON_LANG_PARSER_H_
+
+namespace lang {
+    class parser {
+
+    };
+}  // namespace lang
+
+#endif // !ARGON_LANG_PARSER_H_
