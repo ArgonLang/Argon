5ff9de8d82f69ec1154b25a99c8b341487484d93
jacopodl <jacopo.delu@gmail.com>
2020-02-21 11:36:03 +0100

    Beginning implementation (MemoryManager)

---
+++ src/memory/memory.h
@@ -0,0 +1,12 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#ifndef ARGON_MEMORY_MEMORY_H_
+#define ARGON_MEMORY_MEMORY_H_
+
+namespace argon::memory {
+
+} // namespace argon::memory
+
+#endif // !ARGON_MEMORY_MEMORY_H_
