f02bedb33b536978f9f24fce4263bd91d896fee7
jacopodl <jacopo.delu@gmail.com>
2020-04-07 11:42:59 +0200

    Beginning implementation (Argon Virtual Machine)

---
+++ src/vm/argon_vm.h
@@ -0,0 +1,14 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#ifndef ARGON_VM_ARGON_VM_H_
+#define ARGON_VM_ARGON_VM_H_
+
+namespace argon::vm {
+    class ArgonVM {
+
+    };
+} // namespace argon::vm
+
+#endif // !ARGON_VM_ARGON_VM_H_
