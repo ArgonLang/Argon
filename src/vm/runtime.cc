0bead5a1649152d74557a8de43a2e41bc1f0b49d
jacopodl <jacopo.delu@gmail.com>
2020-08-24 13:53:53 +0200

    Runtime: refactoring, added helper functions for OST and main OSThread func (Schedule)

--- src/vm/runtime.cc
+++ src/vm/runtime.cc
@@ -35,16 +35,23 @@ struct VCore {
     VCoreStatus status;
 };
 
+// Prototypes
+OSThread *AllocOST();
+
+void StartOST(OSThread *);
+
 /* OSThread variables */
-OSThread *ost_active = nullptr; // Working OSThread
-OSThread *ost_idle = nullptr;   // IDLE OSThread
-unsigned int ost_max = 10000;   // Maximum OSThread allowed
-unsigned int ost_count = 0;     // OSThread counter (active + idle)
-std::mutex ost_lock;            // OSThread lock (active + idle)
+OSThread *ost_active = nullptr;             // Working OSThread
+OSThread *ost_idle = nullptr;               // IDLE OSThread
+thread_local OSThread *ost_local = nullptr; //
+unsigned int ost_max = 10000;               // Maximum OSThread allowed
+unsigned int ost_count = 0;                 // OSThread counter
+unsigned int ost_idle_count = 0;            // OSThread counter (idle)
+std::mutex ost_lock;                        // OSThread lock (active + idle)
 
 /* VCore variables */
-VCore *vcs = nullptr;           // List of instantiated VCore
-unsigned int vcs_count = 0;     // Maximum concurrent VCore
+VCore *vcs = nullptr;                       // List of instantiated VCore
+unsigned int vcs_count = 0;                 // Maximum concurrent VCore
 
 bool argon::vm::Initialize() {
     vcs_count = std::thread::hardware_concurrency();
@@ -63,13 +70,10 @@ bool argon::vm::Initialize() {
         vcs[i].queue = ArRoutineQueue();
         vcs[i].status = VCoreStatus::IDLE;
     }
-
     return true;
 }
 
 void PushOSThread(OSThread *ost, OSThread **list) {
-    ost_lock.lock();
-
     if (*list == nullptr) {
         ost->next = nullptr;
         ost->prev = list;
@@ -83,26 +87,79 @@ void PushOSThread(OSThread *ost, OSThread **list) {
     }
 
     *list = ost;
-
-    ost_lock.unlock();
 }
 
-void RemoveOSThread(OSThread *ost) {
-    ost_lock.lock();
-
-    *ost->prev = ost->next;
+inline void RemoveOSThread(OSThread *ost) {
+    if (ost->prev != nullptr)
+        *ost->prev = ost->next;
     if (ost->next != nullptr)
         ost->next->prev = ost->prev;
+}
 
+inline void FromActiveToIdle(OSThread *ost) {
+    ost_lock.lock();
+    RemoveOSThread(ost);
+    PushOSThread(ost, &ost_idle);
+    ost_idle_count++;
     ost_lock.unlock();
 }
 
-inline void MoveOSThreadToActive(OSThread *ost) {
+inline void FromIdleToActive(OSThread *ost) {
+    ost_lock.lock();
     RemoveOSThread(ost);
     PushOSThread(ost, &ost_active);
+    ost_idle_count--;
+    ost_lock.unlock();
 }
 
-inline void MoveOSThreadToIdle(OSThread *ost) {
-    RemoveOSThread(ost);
+bool WireVC() {
+    for (unsigned int i = 0; i < vcs_count; i++) {
+        auto vc = vcs + i;
+        OSThread *current = vc->ost.load(std::memory_order_consume);
+        if (current == nullptr) {
+            if (vc->ost.compare_exchange_weak(current, ost_local, std::memory_order_relaxed)) {
+                ost_local->current = vc;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+void Schedule(OSThread *self) {
+    ost_local = self;
+
+    // Find a free VCore and associate with it
+    while (!WireVC());
+    self->current->status = VCoreStatus::RUNNING;
+    FromIdleToActive(self);
+
+    // DO Things....
+}
+
+OSThread *AllocOST() {
+    auto ost = (OSThread *) argon::memory::Alloc(sizeof(OSThread));
+
+    if (ost != nullptr) {
+        ost->next = nullptr;
+        ost->prev = nullptr;
+
+        ost->routine = nullptr;
+
+        ost->current = nullptr;
+        ost->old = nullptr;
+
+        ost->self = std::thread();
+    }
+
+    return ost;
+}
+
+void StartOST(OSThread *ost) {
+    ost_lock.lock();
     PushOSThread(ost, &ost_idle);
+    ost_count++;
+    ost_idle_count++;
+    ost_lock.unlock();
+    ost->self = std::thread(Schedule, ost);
 }
