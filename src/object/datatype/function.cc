ffeb132249f4a79e14eb6652f938d49b03e7674e
jacopodl <jacopo.delu@gmail.com>
2020-09-08 13:56:18 +0200

    Object: improvements for type Function

--- src/object/datatype/function.cc
+++ src/object/datatype/function.cc
@@ -2,39 +2,69 @@
 //
 // Licensed under the Apache License v2.0
 
+#include <object/objmgmt.h>
 #include "function.h"
 
 using namespace argon::object;
 using namespace argon::memory;
 
-inline void CloneFn(Function *fn_new, const Function *func) {
+Function *CloneFn(const Function *func) {
+    auto fn = ArObjectNew<Function>(RCType::INLINE, &type_function_);
 
-    if (!func->native) {
-        IncRef(func->code);
-        fn_new->code = func->code;
-    } else
-        fn_new->native_fn = func->native_fn;
+    if (fn != nullptr) {
+        if (!func->native) {
+            IncRef(func->code);
+            fn->code = func->code;
+        } else
+            fn->native_fn = func->native_fn;
 
-    IncRef(func->currying);
-    fn_new->currying = func->currying;
+        IncRef(func->currying);
+        fn->currying = func->currying;
 
-    IncRef(func->enclosed);
-    fn_new->enclosed = func->enclosed;
+        IncRef(func->enclosed);
+        fn->enclosed = func->enclosed;
 
-    IncRef(func->instance);
-    fn_new->instance = func->instance;
+        IncRef(func->instance);
+        fn->instance = func->instance;
 
-    fn_new->arity = func->arity;
-    fn_new->variadic = func->variadic;
-    fn_new->native = func->native;
+        fn->arity = func->arity;
+        fn->variadic = func->variadic;
+        fn->native = func->native;
+    }
+
+    return fn;
+}
+
+bool function_equal(ArObject *self, ArObject *other) {
+    if (self->type != other->type)
+        return false;
+
+    if (self != other) {
+        auto l = (Function *) self;
+        auto r = (Function *) other;
+
+        if (l->native_fn == r->native_fn) {
+            if (!l->native && !l->code->type->equal(l->code, r->code))
+                return false;
+
+            return l->instance == r->instance &&
+                   l->currying->type->equal(l->currying, r->currying);
+        }
+
+        return false;
+    }
+    return true;
+}
+
+size_t function_hash(Function *self) {
+    return (size_t) self;
 }
 
 void function_cleanup(Function *fn) {
-    if (!fn->native)
-        Release(fn->code);
-
+    Release(fn->code);
     Release(fn->currying);
     Release(fn->enclosed);
+    Release(fn->instance);
 }
 
 const TypeInfo argon::object::type_function_ = {
@@ -45,9 +75,9 @@ const TypeInfo argon::object::type_function_ = {
         nullptr,
         nullptr,
         nullptr,
+        function_equal,
         nullptr,
-        nullptr,
-        nullptr,
+        (SizeTUnaryOp) function_hash,
         nullptr,
         nullptr,
         (VoidUnaryOp) function_cleanup
@@ -86,12 +116,10 @@ Function *argon::object::FunctionNew(const FunctionNative *native) {
 }
 
 Function *argon::object::FunctionNew(const Function *func, List *currying) {
-    auto fn = ArObjectNew<Function>(RCType::INLINE, &type_function_);
+    auto fn = CloneFn(func);
 
     if (fn != nullptr) {
-
-        CloneFn(fn, func);
-
+        Release(fn->currying);
         IncRef(currying);
         fn->currying = currying;
     }
@@ -100,12 +128,10 @@ Function *argon::object::FunctionNew(const Function *func, List *currying) {
 }
 
 Function *argon::object::FunctionNew(const Function *func, ArObject *instance) {
-    auto fn = ArObjectNew<Function>(RCType::INLINE, &type_function_);
+    auto fn = CloneFn(func);
 
     if (fn != nullptr) {
-
-        CloneFn(fn, func);
-
+        Release(fn->instance); // Actually I expect it to be nullptr!
         IncRef(instance);
         fn->instance = instance;
     }
--- src/object/datatype/function.h
+++ src/object/datatype/function.h
@@ -5,7 +5,7 @@
 #ifndef ARGON_OBJECT_FUNCTION_H_
 #define ARGON_OBJECT_FUNCTION_H_
 
-#include <object/objmgmt.h>
+#include <object/arobject.h>
 #include "code.h"
 
 namespace argon::object {
@@ -13,27 +13,50 @@ namespace argon::object {
     using FunctionNativePtr = ArObject *(*)(struct Function *self, ArObject **argv);
 
     struct FunctionNative {
+        /* Name of native function (this name will be exposed to Argon) */
         const char *name;
+
+        /* Documentation of native function (this doc will be exposed to Argon) */
         const char *doc;
+
+        /* Pointer to native code */
         FunctionNativePtr func;
+
+        /* Arity of the function, how many args accepts in input?! */
         unsigned short arity;
+
+        /* Is a variadic function? (func variadic(p1,p2,...p3))*/
         bool variadic;
     };
 
     struct Function : ArObject {
         union {
+            /* Pointer to Argon code */
             Code *code;
+
+            /* Pointer to structure that contains native code */
             FunctionNativePtr native_fn;
         };
+
+        /* List that contains values for partial application */
         List *currying;
+
+        /* List that contains captured variables in a closure */
         List *enclosed;
 
-        // Method
+        /* This pointer points to a structure instance (if and only if this function is a method) */
         ArObject *instance;
 
+        /* Arity of the function, how many args accepts in input?! */
         unsigned short arity;
+
+        /* Is a variadic function? (func variadic(p1,p2,...p3))*/
         bool variadic;
+
+        /* This function is native? */
         bool native;
+
+        size_t hash;
     };
 
     extern const TypeInfo type_function_;
