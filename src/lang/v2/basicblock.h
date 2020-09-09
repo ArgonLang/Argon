c1f6cd40cf0e92959fbc8b9f2fc1fba744792119
jacopodl <jacopo.delu@gmail.com>
2020-09-09 20:54:33 +0200

    Compiler: added basicblock

--- src/lang/v2/basicblock.h
+++ src/lang/v2/basicblock.h
@@ -5,10 +5,38 @@
 #ifndef ARGON_LANG_BASICBLOCK_H_
 #define ARGON_LANG_BASICBLOCK_H_
 
+#include <cstddef>
+
+#include <lang/opcodes.h>
 
 namespace argon::lang {
     class BasicBlock {
+        size_t allocated_ = 0;
 
+        void CheckSize(size_t size);
+
+    public:
+        BasicBlock *link_next = nullptr;
+
+        struct {
+            BasicBlock *next = nullptr;
+            BasicBlock *jump = nullptr;
+        } flow;
+
+        unsigned char *instr = nullptr;
+        unsigned int instr_sz = 0;
+
+        bool visited = false;
+
+        BasicBlock();
+
+        ~BasicBlock();
+
+        void AddInstr(Instr8 instr8);
+
+        void AddInstr(Instr16 instr16);
+
+        void AddInstr(Instr32 instr32);
     };
 }
 
