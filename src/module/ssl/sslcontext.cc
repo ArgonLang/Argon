198d1c0a7e84209da392ff01736826b7a81625a9
jacopodl <jacopo.delu@gmail.com>
2022-03-24 11:04:47 +0100

    WIP: sslcontext::load_cert_chain

--- src/module/ssl/sslcontext.cc
+++ src/module/ssl/sslcontext.cc
@@ -8,7 +8,10 @@
 
 #include <utils/macros.h>
 
+#include <vm/runtime.h>
+
 #include <object/datatype/error.h>
+#include "object/datatype/function.h"
 #include "object/datatype/nil.h"
 
 #include "ssl.h"
@@ -20,6 +23,101 @@ ARGON_FUNCTION5(sslcontext_, new, "", 1, false) {
     if (!CheckArgs("i:protocol", func, argv, count))
         return nullptr;
 
+    return nullptr;
+}
+
+static int PasswordCallback(char *buf, int size, int rwflag, void *userdata) {
+    auto *obj = (ArObject *) userdata;
+    String *ret;
+    int len;
+
+    if (AR_TYPEOF(obj, type_function_))
+        ret = (String *) argon::vm::Call(obj, 0, nullptr);
+    else
+        ret = (String *) IncRef(obj);
+
+    if (!AR_TYPEOF(ret, type_string_)) {
+        ErrorFormat(type_type_error_, "callback must return a string not '%s'", AR_TYPE_NAME(ret));
+        Release(ret);
+        return -1;
+    }
+
+    len = (int) ret->len;
+
+    if (len > size) {
+        ErrorFormat(type_value_error_, "password cannot be longer than %d bytes", size);
+        Release(ret);
+        return -1;
+    }
+
+    argon::memory::MemoryCopy(buf, ret->buffer, len);
+    Release(ret);
+
+    return len;
+}
+
+ARGON_METHOD5(sslcontext_, load_cert_chain, "", 3, false) {
+    const auto *ctx = (SSLContext *) self;
+    const auto *certfile = (String *) argv[0];
+    const auto *keyfile = (String *) argv[1];
+    auto *callback = argv[2];
+    pem_password_cb *orig_pwd_cb;
+    void *orig_pwd_userdata;
+
+    if (!CheckArgs("s:certfile,s?:keyfile,s*?:password", func, argv, count, type_function_))
+        return nullptr;
+
+    orig_pwd_cb = SSL_CTX_get_default_passwd_cb(ctx->ctx);
+    orig_pwd_userdata = SSL_CTX_get_default_passwd_cb_userdata(ctx->ctx);
+
+    if (IsNull(keyfile))
+        keyfile = certfile;
+
+    if (!IsNull(callback)) {
+        if (!AR_TYPEOF(callback, type_string_) && AR_TYPEOF(callback, type_function_))
+            return ErrorFormat(type_type_error_, "password should be a string or callable");
+
+        SSL_CTX_set_default_passwd_cb(ctx->ctx, PasswordCallback);
+        SSL_CTX_set_default_passwd_cb_userdata(ctx->ctx, callback);
+    }
+
+    if (SSL_CTX_use_certificate_chain_file(ctx->ctx, (const char *) certfile->buffer) != 1) {
+        if (!argon::vm::IsPanicking()) {
+            if (errno != 0)
+                ErrorSetFromErrno();
+            else {
+                // TODO: ssl error
+            }
+        }
+
+        goto ERROR;
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx->ctx, (const char *) keyfile->buffer, SSL_FILETYPE_PEM) != 1) {
+        if (!argon::vm::IsPanicking()) {
+            if (errno != 0)
+                ErrorSetFromErrno();
+            else {
+                // TODO: ssl error
+            }
+        }
+
+        goto ERROR;
+    }
+
+    if (SSL_CTX_check_private_key(ctx->ctx) != 1) {
+        // TODO: ssl error
+        goto ERROR;
+    }
+
+    SSL_CTX_set_default_passwd_cb(ctx->ctx, orig_pwd_cb);
+    SSL_CTX_set_default_passwd_cb_userdata(ctx->ctx, orig_pwd_userdata);
+
+    return ARGON_OBJECT_NIL;
+
+    ERROR:
+    SSL_CTX_set_default_passwd_cb(ctx->ctx, orig_pwd_cb);
+    SSL_CTX_set_default_passwd_cb_userdata(ctx->ctx, orig_pwd_userdata);
     return nullptr;
 }
 
@@ -40,6 +138,7 @@ ARGON_METHOD5(sslcontext_, load_certs_default, "", 1, false) {
 
 const NativeFunc sslcontext_methods[] = {
         sslcontext_new_,
+        sslcontext_load_cert_chain_,
         sslcontext_load_certs_default_,
         ARGON_METHOD_SENTINEL
 };
@@ -87,7 +186,7 @@ const TypeInfo SSLContextType = {
 const TypeInfo *argon::module::ssl::type_sslcontext_ = &SSLContextType;
 
 SSLContext *SSLContextNew(SSLProtocol protocol) {
-    const SSL_METHOD *method = nullptr;
+    const SSL_METHOD *method;
     SSLContext *ctx;
 
     switch (protocol) {
@@ -100,10 +199,9 @@ SSLContext *SSLContextNew(SSLProtocol protocol) {
         case SSLProtocol::TLS_SERVER:
             method = TLS_server_method();
             break;
+        default:
+            return (SSLContext *) ErrorFormat(type_value_error_, "invalid protocol %i", protocol);
     }
-
-    if (method == nullptr)
-        return (SSLContext *) ErrorFormat(type_value_error_, "invalid protocol %i", protocol);
 
     if ((ctx = ArObjectNew<SSLContext>(RCType::INLINE, type_sslcontext_)) == nullptr)
         return nullptr;
@@ -113,8 +211,6 @@ SSLContext *SSLContextNew(SSLProtocol protocol) {
         // TODO: ERROR
         return nullptr;
     }
-
-    SSL_CTX_set_default_verify_paths(ctx->ctx);
 
     return ctx;
 }
