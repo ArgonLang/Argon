2cddf73bca1f06ed1949c66621871275daf61e9c
jacopodl <jacopo.delu@gmail.com>
2022-08-03 12:50:42 +0200

    Add STUB Runtime

---
+++ src/vm/runtime.cpp
@@ -0,0 +1,12 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#include "runtime.h"
+
+using namespace argon::vm;
+
+void argon::vm::Panic(datatype::ArObject *panic) {
+    // TODO: STUB
+    assert(false);
+}
---
+++ src/vm/runtime.h
@@ -0,0 +1,14 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#ifndef ARGON_VM_RUNTIME_H_
+#define ARGON_VM_RUNTIME_H_
+
+#include "datatype/arobject.h"
+
+namespace argon::vm {
+    void Panic(datatype::ArObject *panic);
+}
+
+#endif // !ARGON_VM_RUNTIME_H_
