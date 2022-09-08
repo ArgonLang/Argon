49f1b63319f7eebd7fd11efacd1f6d598585c749
jacopodl <jacopo.delu@gmail.com>
2022-09-08 10:19:53 +0200

    Add IntegerCountBits and IntegerCountDigits

--- src/vm/datatype/integer.h
+++ src/vm/datatype/integer.h
@@ -22,6 +22,40 @@ namespace argon::vm::datatype {
     extern const TypeInfo *type_int_;
     extern const TypeInfo *type_uint_;
 
+    inline bool IsIntType(const ArObject *object) {
+        return AR_TYPEOF(object, type_int_) || AR_TYPEOF(object, type_uint_);
+    }
+
+    template<typename T>
+    int IntegerCountBits(T number) {
+        int count = 0;
+
+        if (number < 0)
+            number *= -1;
+
+        while (number) {
+            count++;
+            number >>= 1u;
+        }
+
+        return count;
+    }
+
+    template<typename T>
+    int IntegerCountDigits(T number, int base) {
+        int count = 0;
+
+        if (number == 0)
+            return 1;
+
+        while (number) {
+            count++;
+            number /= base;
+        }
+
+        return count;
+    }
+
     Integer *IntNew(IntegerUnderlying number);
 
     Integer *IntNew(const char *string, int base);
