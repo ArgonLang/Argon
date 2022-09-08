9c58118b36004392f4997199eb91fecb8eefe1ce
jacopodl <jacopo.delu@gmail.com>
2022-09-08 10:41:25 +0200

    Bugfix

---
+++ src/util/enum_bitmask.h
@@ -0,0 +1,76 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#include <type_traits>
+
+#ifndef ARGON_UTIL_ENUMBITMASK_H_
+#define ARGON_UTIL_ENUMBITMASK_H_
+
+template<typename Enum>
+struct EnableBitMaskOps {
+    static const bool enable = false;
+};
+
+#define ENUMBITMASK_ISTRUE(l, r)    (((l) & (r)) == r)
+#define ENUMBITMASK_ISFALSE(l, r)   (((l) & (r)) != r)
+#define ENUMBITMASK_ENABLE(x)       \
+template<>                          \
+struct EnableBitMaskOps<x> {static const bool enable = true;}
+
+#define _ENUMBITMASK_ASSERT         static_assert(std::is_enum<Enum>::value, "template parameter must be an enum class")
+#define _ENUMBITMASK_UNDERLYING     using underlying = typename std::underlying_type<Enum>::type
+
+template<typename Enum>
+typename std::enable_if<EnableBitMaskOps<Enum>::enable, Enum>::type operator~(Enum rhs) {
+    _ENUMBITMASK_ASSERT;
+    _ENUMBITMASK_UNDERLYING;
+    return static_cast<Enum>(~static_cast<underlying>(rhs));
+}
+
+template<typename Enum>
+typename std::enable_if<EnableBitMaskOps<Enum>::enable, Enum>::type operator&(Enum lhs, Enum rhs) {
+    _ENUMBITMASK_ASSERT;
+    _ENUMBITMASK_UNDERLYING;
+    return static_cast<Enum>(static_cast<underlying>(lhs) & static_cast<underlying>(rhs));
+}
+
+template<typename Enum>
+typename std::enable_if<EnableBitMaskOps<Enum>::enable, Enum>::type operator^(Enum lhs, Enum rhs) {
+    _ENUMBITMASK_ASSERT;
+    _ENUMBITMASK_UNDERLYING;
+    return static_cast<Enum>(static_cast<underlying>(lhs) ^ static_cast<underlying>(rhs));
+}
+
+template<typename Enum>
+typename std::enable_if<EnableBitMaskOps<Enum>::enable, Enum>::type operator|(Enum lhs, Enum rhs) {
+    _ENUMBITMASK_ASSERT;
+    _ENUMBITMASK_UNDERLYING;
+    return static_cast<Enum>(static_cast<underlying>(lhs) | static_cast<underlying>(rhs));
+}
+
+template<typename Enum>
+typename std::enable_if<EnableBitMaskOps<Enum>::enable, Enum>::type &operator&=(Enum &lhs, Enum rhs) {
+    _ENUMBITMASK_ASSERT;
+    _ENUMBITMASK_UNDERLYING;
+    lhs = static_cast<Enum>(static_cast<underlying>(lhs) & static_cast<underlying>(rhs));
+    return lhs;
+}
+
+template<typename Enum>
+typename std::enable_if<EnableBitMaskOps<Enum>::enable, Enum>::type &operator^=(Enum &lhs, Enum rhs) {
+    _ENUMBITMASK_ASSERT;
+    _ENUMBITMASK_UNDERLYING;
+    lhs = static_cast<Enum>(static_cast<underlying>(lhs) ^ static_cast<underlying>(rhs));
+    return lhs;
+}
+
+template<typename Enum>
+typename std::enable_if<EnableBitMaskOps<Enum>::enable, Enum>::type &operator|=(Enum &lhs, Enum rhs) {
+    _ENUMBITMASK_ASSERT;
+    _ENUMBITMASK_UNDERLYING;
+    lhs = static_cast<Enum>(static_cast<underlying>(lhs) | static_cast<underlying>(rhs));
+    return lhs;
+}
+
+#endif // !ARGON_UTIL_ENUMBITMASK_H_
---
+++ src/util/macros.h
@@ -0,0 +1,46 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#ifndef ARGON_UTIL_MACROS_H_
+#define ARGON_UTIL_MACROS_H_
+
+// from <bits/wordsize.h>
+#ifndef __WORDSIZE
+#if (defined __x86_64__ && !defined __ILP32__) || defined __LP64__
+# define __WORDSIZE	64
+#else
+# define __WORDSIZE	32
+#endif
+#endif
+
+// WORD SIZE
+#if defined _WIN64 || __WORDSIZE == 64
+#define _ARGON_ENVIRON 64
+#elif defined(_WIN32) || __WORDSIZE == 32
+#define _ARGON_ENVIRON 32
+#endif
+
+// OS PLATFORM
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#define _ARGON_PLATFORM_WINDOWS
+#define _ARGON_PLATFORM_NAME "windows"
+#define _ARGONAPI __declspec(dllimport)
+#elif defined(__APPLE__)
+#define _ARGON_PLATFORM_DARWIN
+#define _ARGON_PLATFORM_NAME "darwin"
+#define _ARGONAPI
+#elif defined(__linux__)
+#define _ARGON_PLATFORM_LINUX
+#define _ARGON_PLATFORM_NAME "linux"
+#define _ARGONAPI
+#elif defined(__unix__)
+#define _ARGON_PLATFORM_UNIX
+#define _ARGON_PLATFORM_NAME "unix"
+#define _ARGONAPI
+#else
+#define _ARGON_PLATFORM_NAME "unknown"
+#define _ARGONAPI
+#endif
+
+#endif // !ARGON_UTIL_MACROS_H_
