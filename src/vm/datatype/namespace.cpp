c5006e5ae86dd8eb85231b9cf6019f8c7a60a7cb
jacopodl <jacopo.delu@gmail.com>
2022-09-10 21:14:49 +0200

    Add Namespace

---
+++ src/vm/datatype/namespace.cpp
@@ -0,0 +1,127 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#include <cassert>
+
+#include "arstring.h"
+#include "nil.h"
+#include "namespace.h"
+
+using namespace argon::vm::datatype;
+
+bool NewEntry(Namespace *ns, ArObject *key, ArObject *value, AttributeFlag aa) {
+    NSEntry *entry = ns->ns.Lookup(key);
+
+    if (entry != nullptr) {
+        entry->value.value.Store(value, ENUMBITMASK_ISTRUE(aa, AttributeFlag::WEAK));
+        entry->value.properties.flags = aa;
+        return true;
+    }
+
+    if ((entry = ns->ns.AllocHEntry()) == nullptr)
+        return false;
+
+    entry->key = IncRef(key);
+
+    entry->value.value.Store(value, ENUMBITMASK_ISTRUE(aa, AttributeFlag::WEAK));
+    entry->value.properties.flags = aa;
+
+    if (!ns->ns.Insert(entry)) {
+        Release(key);
+
+        entry->value.value.Release();
+
+        ns->ns.FreeHEntry(entry);
+    }
+
+    return true;
+}
+
+const TypeInfo NamespaceType = {
+        AROBJ_HEAD_INIT_TYPE,
+        "Namespace",
+        nullptr,
+        nullptr,
+        sizeof(Namespace),
+        TypeInfoFlags::BASE,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr
+};
+const TypeInfo *argon::vm::datatype::type_namespace_ = &NamespaceType;
+
+bool argon::vm::datatype::NamespaceContains(Namespace *ns, ArObject *key, AttributeProperty *out_aprop) {
+    const NSEntry *entry = ns->ns.Lookup(key);
+
+    if (entry != nullptr) {
+        *out_aprop = entry->value.properties;
+        return true;
+    }
+
+    return false;
+}
+
+bool argon::vm::datatype::NamespaceNewSymbol(Namespace *ns, ArObject *key, ArObject *value, AttributeFlag aa) {
+    assert(AR_GET_TYPE(key)->hash != nullptr);
+
+    if (value == nullptr)
+        value = (ArObject *) IncRef(Nil);
+
+    return NewEntry(ns, key, value, aa);
+}
+
+bool argon::vm::datatype::NamespaceNewSymbol(Namespace *ns, const char *key, ArObject *value, AttributeFlag aa) {
+    auto *str = StringIntern(key);
+    if (str == nullptr)
+        return false;
+
+    auto ok = NamespaceNewSymbol(ns, (ArObject *) str, value, aa);
+
+    Release(str);
+
+    return ok;
+}
+
+bool argon::vm::datatype::NamespaceSet(Namespace *ns, ArObject *key, ArObject *value) {
+    NSEntry *entry;
+
+    assert(AR_GET_TYPE(key)->hash != nullptr);
+
+    if ((entry = ns->ns.Lookup(key)) != nullptr) {
+        entry->value.value.Store(value);
+        return true;
+    }
+
+    return false;
+}
+
+Namespace *argon::vm::datatype::NamespaceNew() {
+    auto *ns = MakeGCObject<Namespace>(&NamespaceType);
+
+    if (ns != nullptr && !ns->ns.Initialize())
+        Release(ns);
+
+    return ns;
+}
+
+void argon::vm::datatype::NamespaceClear(Namespace *ns) {
+    ns->ns.Clear([](NSEntry *entry) {
+        Release(entry->key);
+        entry->value.value.Release();
+    });
+}
---
+++ src/vm/datatype/namespace.h
@@ -0,0 +1,116 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#ifndef ARGON_VM_DATATYPE_NAMESPACE_H_
+#define ARGON_VM_DATATYPE_NAMESPACE_H_
+
+#include <util/macros.h>
+
+#include "arobject.h"
+#include "hashmap.h"
+
+namespace argon::vm::datatype {
+    enum class AttributeFlag {
+        // Behaviour
+        CONST = 1,
+        WEAK = 1 << 1,
+
+        // Visibility
+        PUBLIC = 1 << 2
+    };
+}
+
+ENUMBITMASK_ENABLE(argon::vm::datatype::AttributeFlag);
+
+namespace argon::vm::datatype {
+    struct AttributeProperty {
+        AttributeFlag flags;
+
+        [[nodiscard]] bool IsConstant() const {
+            return ENUMBITMASK_ISTRUE(this->flags, AttributeFlag::CONST);
+        };
+
+        [[nodiscard]] bool IsPublic() const {
+            return ENUMBITMASK_ISTRUE(this->flags, AttributeFlag::PUBLIC);
+        }
+
+        [[nodiscard]] bool IsWeak() const {
+            return ENUMBITMASK_ISTRUE(this->flags, AttributeFlag::WEAK);
+        }
+    };
+
+    struct PropertyStore {
+        RefStore value;
+
+        AttributeProperty properties;
+    };
+
+    using NSEntry = HEntry<ArObject, PropertyStore>;
+
+    struct Namespace {
+        AROBJ_HEAD;
+
+        HashMap<ArObject, PropertyStore> ns;
+    };
+    extern const TypeInfo *type_namespace_;
+
+    /**
+     * @brief Checks if the namespace contains the key and returns its AttributeProperty.
+     *
+     * @param ns Pointer to namespace.
+     * @param key Pointer to the key.
+     * @param out_aprop Pointer to an AttributeProperty writable object.
+     * @return True if key exists, false otherwise.
+     */
+    bool NamespaceContains(Namespace *ns, ArObject *key, AttributeProperty *out_aprop);
+
+    /**
+     * @brief Add new symbol to namespace.
+     *
+     * @param ns Pointer to namespace.
+     * @param key Pointer to the key.
+     * @param value Pointer to the value to be added.
+     * @param aa AttributeFlag
+     * @return True on success, false otherwise.
+     */
+    bool NamespaceNewSymbol(Namespace *ns, ArObject *key, ArObject *value, AttributeFlag aa);
+
+    /**
+     * @brief Add new symbol to namespace.
+     *
+     * @param ns Pointer to namespace.
+     * @param key Pointer to the C-string that represent the key.
+     * @param value Pointer to the value to be added.
+     * @param aa AttributeFlag
+     * @return True on success, false otherwise.
+     */
+    bool NamespaceNewSymbol(Namespace *ns, const char *key, ArObject *value, AttributeFlag aa);
+
+    /**
+     * @brief Replaces the value associated with a key.
+     *
+     * @param ns Pointer to namespace.
+     * @param key Pointer to the key.
+     * @param value Pointer to the value to be added.
+     * @return True if the value has been replaced, false otherwise (The key does not exist).
+     */
+    bool NamespaceSet(Namespace *ns, ArObject *key, ArObject *value);
+
+    /**
+     * @brief Create new namespace.
+     *
+     * @return A pointer to new namespace, otherwise nullptr.
+     */
+    Namespace *NamespaceNew();
+
+    /**
+     * @brief Cleans the namespace.
+     *
+     * @param ns Pointer to namespace.
+     */
+    void NamespaceClear(Namespace *ns);
+
+} // namespace argon::vm::datatype
+
+#endif // !ARGON_VM_DATATYPE_NAMESPACE_H_
