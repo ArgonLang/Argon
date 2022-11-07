111968db35f2c815a42ee95b2614e6fa440cba7c
jacopodl <jacopo.delu@gmail.com>
2022-11-07 13:55:41 +0100

    Add Module type

---
+++ src/vm/datatype/module.h
@@ -0,0 +1,60 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#ifndef ARGON_DATATYPE_MODULE_H_
+#define ARGON_DATATYPE_MODULE_H_
+
+#include "arstring.h"
+#include "namespace.h"
+#include "arobject.h"
+
+namespace argon::vm::datatype {
+
+#define MODULE_ATTRIBUTE_DEFAULT (AttributeFlag::CONST|AttributeFlag::PUBLIC)
+
+    using ModuleInitFn = bool (*)(struct Module *);
+    using ModuleFiniFn = bool (*)(struct Module *);
+
+    struct Module {
+        AROBJ_HEAD;
+
+        Namespace *ns;
+
+        ModuleFiniFn fini;
+    };
+    extern const TypeInfo *type_module_;
+
+    struct ModuleEntry {
+        const char *name;
+
+        union {
+            ArObject *object;
+            FunctionDef *func;
+        } prop;
+
+        bool func;
+        AttributeFlag flags;
+    };
+
+    struct ModuleInit {
+        const char *name;
+        const char *doc;
+
+        const ModuleEntry *bulk;
+
+        ModuleInitFn init;
+        ModuleFiniFn fini;
+    };
+
+    ArObject *ModuleLookup(const Module *mod, const char *key, AttributeProperty *out_prop);
+
+    Module *ModuleNew(ModuleInit *init);
+
+    Module *ModuleNew(String *name, String *doc);
+
+    Module *ModuleNew(const char *name, const char *doc);
+
+} // namespace argon::vm::datatype
+
+#endif // !ARGON_DATATYPE_MODULE_H_
