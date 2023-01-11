f48c6eced4d3f468374d9bc707467d1b109e264d
jacopodl <jacopo.delu@gmail.com>
2023-01-11 15:05:10 +0100

    Add support for 'dlwrap' in Windows environment

--- src/vm/importer/dlwrap.cpp
+++ src/vm/importer/dlwrap.cpp
@@ -2,22 +2,43 @@
 //
 // Licensed under the Apache License v2.0
 
-#include <vm/datatype/error.h>
-
-#include "dlwrap.h"
+#include <util/macros.h>
 
 #ifdef _ARGON_PLATFORM_WINDOWS
-// TODO Windows
+
+#include <Windows.h>
+
 #else
 
 #include <dlfcn.h>
 
 #endif
 
+#include <vm/datatype/error.h>
+
+#include "dlwrap.h"
+
 using namespace argon::vm::datatype;
 using namespace argon::vm::importer;
 
 DLHandle argon::vm::importer::OpenLibrary(const char *path, Error **out_error) {
+#ifdef _ARGON_PLATFORM_WINDOWS
+    HMODULE handle;
+
+    if ((handle = LoadLibraryA(path)) == nullptr) {
+        auto *winerr = ErrorGetMsgFromWinErr();
+
+        *out_error = ErrorNewFormat(kModuleImportError[0],
+                                    "NativeImportError: %s",
+                                    winerr != nullptr ? ARGON_RAW_STRING(winerr) : (unsigned char *) "");
+
+        Release(winerr);
+
+        return nullptr;
+    }
+
+    return (DLHandle) handle;
+#else
     DLHandle handle;
 
     *out_error = nullptr;
@@ -29,13 +50,31 @@ DLHandle argon::vm::importer::OpenLibrary(const char *path, Error **out_error) {
     }
 
     return handle;
+#endif
 }
 
 DLHandle argon::vm::importer::LoadSymbol(DLHandle handle, const char *sym_name) {
+#ifdef _ARGON_PLATFORM_WINDOWS
+    return (DLHandle) GetProcAddress((HMODULE) handle, sym_name);
+#else
     return dlsym(handle, sym_name);
+#endif
 }
 
 int argon::vm::importer::CloseLibrary(DLHandle handle, datatype::Error **out_error) {
+#ifdef _ARGON_PLATFORM_WINDOWS
+    if (FreeLibrary((HMODULE) handle) == 0) {
+        auto *winerr = ErrorGetMsgFromWinErr();
+
+        *out_error = ErrorNewFormat(kModuleImportError[0],
+                                    "UnloadNativeModule: %s",
+                                    winerr != nullptr ? ARGON_RAW_STRING(winerr) : (unsigned char *) "");
+
+        Release(winerr);
+
+        return 1;
+    }
+#else
     *out_error = nullptr;
 
     if (dlclose(handle) != 0) {
@@ -43,6 +82,7 @@ int argon::vm::importer::CloseLibrary(DLHandle handle, datatype::Error **out_err
 
         return 1;
     }
+#endif
 
     return 0;
 }
--- src/vm/importer/dlwrap.h
+++ src/vm/importer/dlwrap.h
@@ -5,12 +5,7 @@
 #ifndef ARGON_VM_IMPORTER_DLWRAP_H_
 #define ARGON_VM_IMPORTER_DLWRAP_H_
 
-#include <util/macros.h>
-
 namespace argon::vm::importer {
-#ifdef _ARGON_PLATFORM_WINDOWS
-    // TODO: Windows
-#else
     using DLHandle = void *;
 
     constexpr DLHandle DLHandleError = nullptr;
@@ -20,8 +15,6 @@ namespace argon::vm::importer {
     DLHandle LoadSymbol(DLHandle handle, const char *sym_name);
 
     int CloseLibrary(DLHandle handle, datatype::Error **out_error);
-
-#endif
 
 } // namespace argon::vm::importer
 
